7de6cd0acd6b342635aa0fd11732f7662d87bda5
LesGibbs307 <32602312+LesGibbs307@users.noreply.github.com>
2019-07-07 15:54:18 -0400

    Delete KeyValuePairModelDescription.cs

--- RMI.MondayComDashboard/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace RMI.MondayComDashboard.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
